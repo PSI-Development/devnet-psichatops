1337406fabcbc3bcffa070f21e97ac55a931764a
wisnumur <wisnu_murdianto@yahoo.com>
2021-05-18 14:27:10 +0700

    commit pertama

---
+++ app.py
@@ -0,0 +1,111 @@
+
+import os
+import requests
+from webexteamsbot import TeamsBot
+from webexteamsbot.models import Response
+import sys
+import json
+from dnac_manager import DNACManager
+'''
+os.environ["TEAMS_BOT_EMAIL"] = "psi-chatops@webex.bot"
+os.environ["TEAMS_BOT_TOKEN"] = "NTA0MDczM2EtMzI0YS00MjgxLWEyNzQtMzNkMDRhMThhNWUxZjNhNzdiMWItZWZj_PF84_8992f87e-6618-4a3c-b512-1b3b50b6f6f3"
+os.environ["TEAMS_BOT_URL"] = "http://222.165.234.92"
+os.environ["TEAMS_BOT_APP_NAME"] = "psi-chatops"
+'''
+# Initialized managed entitities (e.g. DNAC, APIC, IOS, SolarWind)
+dnac = DNACManager()
+
+# Retrieve required details from environment variables
+bot_email = os.getenv("TEAMS_BOT_EMAIL")
+teams_token = os.getenv("TEAMS_BOT_TOKEN")
+bot_url = os.getenv("TEAMS_BOT_URL")
+bot_app_name = os.getenv("TEAMS_BOT_APP_NAME")
+
+# If any of the bot environment variables are missing, terminate the app
+if not bot_email or not teams_token or not bot_url or not bot_app_name:
+    print(
+        "sample.py - Missing Environment Variable. Please see the 'Usage'"
+        " section in the README."
+    )
+    if not bot_email:
+        print("TEAMS_BOT_EMAIL")
+    if not teams_token:
+        print("TEAMS_BOT_TOKEN")
+    if not bot_url:
+        print("TEAMS_BOT_URL")
+    if not bot_app_name:
+        print("TEAMS_BOT_APP_NAME")
+    sys.exit()
+
+# Create a Bot Object
+#   Note: debug mode prints out more details about processing to terminal
+#   Note: the `approved_users=approved_users` line commented out and shown as reference
+bot = TeamsBot(
+    bot_app_name,
+    teams_bot_token=teams_token,
+    teams_bot_url=bot_url,
+    teams_bot_email=bot_email,
+    debug=True,
+    # approved_users=approved_users,
+    webhook_resource_event=[
+        {"resource": "messages", "event": "created"},
+        {"resource": "attachmentActions", "event": "created"},
+    ],
+)
+
+# Create a custom bot greeting function returned when no command is given.
+# The default behavior of the bot is to return the '/help' command response
+def greeting(incoming_msg):
+    # Loopkup details about sender
+    sender = bot.teams.people.get(incoming_msg.personId)
+
+    # Create a Response object and craft a reply in Markdown.
+    response = Response()
+    response.markdown = "Hello {}, I'm a psi-chatops bot. ".format(sender.firstName)
+    response.markdown += "See what I can do by asking for **/help**."
+    return response
+
+
+# Create functions that will be linked to bot commands to add capabilities
+# ------------------------------------------------------------------------
+
+# A simple command that returns a basic string that will be sent as a reply
+def hello_webex(incoming_msg):
+    """
+    Sample function to do some action.
+    :param incoming_msg: The incoming message object from Teams
+    :return: A text or markdown based reply
+    """
+    return "Hello  Webex from Python  - {}".format(incoming_msg.text)
+
+def device_list(incoming_msg):
+    msg = dnac.device_list()
+    print(msg)
+    return msg
+
+def device_topology(incoming_msg):
+    return "Device Topology - DNAC"
+
+def export_inventory(incoming_msg):
+    return "Export Inventory - DNAC"
+
+def cmd_run(incoming_msg):
+    return "Command Runner - DNAC"
+
+# Set the bot greeting.
+bot.set_greeting(greeting)
+
+# Add new commands to the bot.
+bot.add_command("/hello-webex", "Say Hello to Webex Teams", hello_webex)
+bot.add_command("/device-list", "DNAC Device Inventory List", device_list)
+bot.add_command("/device-topology", "DNAC Physical Network Topology", device_topology)
+bot.add_command("/export-inventory", "DNAC export Inventory Report", export_inventory)
+bot.add_command("/cmd-run", "DNAC Command Runner Tools", cmd_run)
+
+# Every bot includes a default "/echo" command.  You can remove it, or any
+# other command with the remove_command(command) method.
+bot.remove_command("/echo")
+
+if __name__ == "__main__":
+    # Run Bot
+    bot.run(host="0.0.0.0", port=5000)
---
+++ dnac_manager.py
@@ -0,0 +1,68 @@
+from dnacentersdk import api , ApiError, dnacentersdkException
+import requests
+import urllib3
+requests.packages.urllib3.disable_warnings() 
+import json     
+import sys
+from graphviz import Digraph
+import json
+from pandas import DataFrame
+import pandas as pd
+import os
+import time
+from requests.auth import HTTPBasicAuth
+import sqlite3 as db
+from xlsxwriter.workbook import Workbook
+from datetime import datetime , timedelta
+
+
+class DNACManager():
+
+    def __init__(self):
+        self.dnac = self._connect()
+        if self.dnac is not None:
+            print('DNAC Initialization Successful')
+        else:
+            print('DNAC Initialization fails')
+
+    def _connect(self):
+        dnac_uname = os.environ["DNAC_USERNAME"]
+        dnac_pass =os.environ["DNAC_PASSWORD"]
+        dnac_url = os.environ["DNAC_CONN"]
+        print(dnac_uname,dnac_pass,dnac_url)
+        dnac = api.DNACenterAPI(base_url=dnac_url, username=dnac_uname, password=dnac_pass, verify=False)
+        if dnac is not None:
+            return dnac
+        else:
+            print('dnac client fails to connect')
+            return None
+
+    def device_list(self):
+        try :
+            device_list = self.dnac.devices.get_device_list()
+            #print(device_list)
+            msg_content_list = []
+            title_mrkdown = ("Device List in DNAC are: \n")
+            device_idx = 1
+            for device in device_list['response']:
+                    if device is not None:
+                                    #print(device)
+                                    site_mrkdown = ("*{}. Hostname:* {} ".format(device_idx, device['hostname']))
+                                    site_mrkdown_status = ( "`IpAddress: {}`  `platformId: {}` `macAddress: {}`".format(device['managementIpAddress'], device['platformId'],device['macAddress']))
+                                    msg_content_list.append(site_mrkdown)
+                                    msg_content_list.append(site_mrkdown_status)
+                                    #print(msg_content_list)
+                                    device_idx+=1
+            msg = msg_content_list
+            #print(msg)
+            return(msg)
+        except (ApiError, dnacentersdkException) as e:
+            print(e)
+            msg = "DNAC cannot be access currently"
+            return(msg)
+
+'''
+dnac = DNACManager()
+msg = dnac.device_list()
+print(msg)
+'''
